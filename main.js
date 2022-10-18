5f408fee53b7f933fa459dcd53a82bca504e3bd6
Eliseo Avila <ebae82@hotmail.com>
2022-10-18 17:19:55 -0300

    Ajustando detalle de eventos de teclado

--- main.js
+++ main.js
@@ -2,31 +2,33 @@ const buttons = document.querySelectorAll('.buttons_btn')
 const comment = document.getElementById('comment')
 const userPhoto = document.getElementById('user_photo')
 const userName = document.getElementById('user_name')
+const events = ['click', 'keyup']
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
+events.forEach(function (event) {
+    window.addEventListener(event, () => {
+        // console.log(event)
         if (!comment.classList.contains('comment')) {
             comment.classList.add('comment')
             comment.innerHTML = `
-            “ If you want to lay the best foundation possible I’d recommend taking this course. 
-            The depth the instructors go into is incredible. I now feel so confident about 
-            starting up as a professional developer. ”
-            `
+                “ If you want to lay the best foundation possible I’d recommend taking this course. 
+                The depth the instructors go into is incredible. I now feel so confident about 
+                starting up as a professional developer. ”
+                `
             userPhoto.classList.add('change_photo_user')
             userName.innerHTML = `
-                John Tarkpor<span class="content_comment_user_position" id="user_position">Junior Front-end Developer</span>
-            `
+                    John Tarkpor<span class="content_comment_user_position" id="user_position">Junior Front-end Developer</span>
+                `
         } else {
             comment.classList.remove('comment')
             comment.innerHTML = `
-            “ I’ve been interested in coding for a while but never taken the jump, until now. 
-            I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
-            excited about the future. ”
-            `
+                “ I’ve been interested in coding for a while but never taken the jump, until now. 
+                I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
+                excited about the future. ”
+                `
             userPhoto.classList.remove('change_photo_user')
             userName.innerHTML = `
-                Tanya Sinclair<span class="content_comment_user_position" id="user_position">UX Engineer</span>
-            `
+                    Tanya Sinclair<span class="content_comment_user_position" id="user_position">UX Engineer</span>
+                `
         }
         anime({
             targets: '.content_comment',
@@ -35,7 +37,7 @@ buttons.forEach(button => {
                 duration: 800
             },
             easing: 'easeOutExpo'
-        });
+        })
         anime({
             targets: '.user_image_photo',
             opacity: {
@@ -47,7 +49,7 @@ buttons.forEach(button => {
                 duration: 1000
             },
             easing: 'easeOutExpo'
-        });
+        })
         anime({
             targets: '.content_comment_user',
             opacity: {
@@ -60,8 +62,8 @@ buttons.forEach(button => {
                 delay: 100
             },
             easing: 'easeOutExpo'
-        });
+        })
     })
-});
+})
 
 
